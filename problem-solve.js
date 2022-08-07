ac1b33fa50c6679f1b6c150dbb2b8f902171433d
ashr-aful007 <ashraful99980@gmail.com>
2022-08-07 23:54:17 +0600

    problem solving basic

---
+++ problem-solve.js
@@ -0,0 +1,331 @@
+// 1.problem
+
+// var giveHisMom = 1000;
+// var apple = 400;
+// var orange = 300;
+//total apple and orange price
+// var toatalCost = apple + orange;
+// console.log(toatalCost)
+//back amount claclution
+// var backMony = giveHisMom - toatalCost;
+// console.log(backMony)
+
+// 2.problem
+//subject number
+// var subNumber = [75.25, 65.80, 35.45, 99.50];
+// var totalNumber = 75.25 + 65.80 + 35.45 + 99.50;
+// console.log(totalNumber)
+//average number 
+// var averageNumber = totalNumber / 5;
+// console.log(averageNumber)
+//convert intiger Number
+//  var averageNumberConvert = parseInt(averageNumber);
+//  console.log(averageNumberConvert)
+
+//3.problem
+// var frstLine = ['i am going to be'];
+// var secendLine = ['an awesome web developer'];
+// console.log(frstLine+' '+secendLine)
+
+//4.problem
+// var mainNumber = 119;
+// var dividedNumber = 5;
+//remainder number
+// var remainderNumber = mainNumber%dividedNumber;
+// console.log(remainderNumber)
+
+//5.problem
+// var arry = ['banana', 'orange']
+// console.log(positionIndex)
+// var positionIndex = arry.indexOf('banana')
+// arry[0] = 'mango'
+//cut dawn orange
+// var replese = arry.splice(1,1)
+//add items in arry
+// arry.push('watermelon')
+// console.log(arry)
+
+//6.problem
+// var number = 95;
+// if(number<=100 && number>=80){
+//      console.log('congress you got A grade')
+// }
+// else if(number<=79 && number>=60){
+//      console.log('congress you got B grade')
+// }
+// else if(number<=59 && number>= 40){
+//      console.log('congress you got C grade')
+// }
+// else if(number<=39 && number){
+
+// }
+
+//6. problem solve epictively
+// var frinedScore = [85, 66, 95, 56, 40];
+// var friendsName = ['tom', 'janne', 'peter', 'jhon'];
+
+// for(var i =0, j=0; i<frinedScore.length; i++, j++){
+//      if(frinedScore[i]>=80){
+//           console.log(friendsName[j],'Grade is A');
+//      }
+//      else if(frinedScore[i]>=60){
+//           console.log(friendsName[j],'Gride is B');
+//      }
+//      else if(frinedScore[i]>=50){
+//           console.log(friendsName[j], 'Gride is C');
+//      }
+//      else if(frinedScore[i]>=40){
+//           console.log(friendsName[j],'Gride is D');
+//      }
+//      else if(frinedScore[i]>=39){
+//           console.log(friendsName[j], 'Gride F')
+//      }
+//      else{
+//           console.log(friendsName[j],'your are faill')
+//      }
+// }
+
+//7.problem
+// var item1 = 13;
+// var item2 = 79;
+// var item3 = 45;
+// if(item1>item2){
+//      console.log(item1, '13 is begger than other')
+// }
+// else if(item2>item1){
+//      console.log(item2, '79 is begger than other')
+
+// }
+// else if(item3>item2){
+//      console.log(item3, '45 is begger than other')
+// }
+// else if(item3<item1){
+//      console.log(item3, '13 is begger than other')
+// }
+// else{
+//      console.log('no one is the bigger')
+// }
+
+//8.problem
+// var angleOne = 9;
+// var angleTwo = 8;
+// var angleThree = 9;
+
+// if(angleOne>=angleTwo){
+//      console.log('it is triangle')
+// }
+// else if(angleTwo>=angleThree){
+//      console.log('it is triangle')
+// }
+// else if(angleOne>=angleThree){
+//      console.log('it is triangle')
+// }
+// else{
+//      console.log('it is not triangle')
+// }
+
+//6.problem
+// var num = 1;
+// while(num <= 10){
+//      console.log(num);
+//      num++;
+// }
+// for(var i= 1; i <= 10; i++){
+//      console.log(i);
+//      if(i == 5){
+//           break;
+//      }
+// }
+// 7.problem
+// var taka = 50000;
+// if(taka<= 80000){
+//      console.log('I can bay laptop')
+// }
+// else if(taka<= 60000){
+//      console.log('i can bay gaming laptop')
+// }
+// else if(taka<= 40000){
+//      console.log('i can bay lenovo youga')
+// }
+// else if(taka<= 20000){
+//      console.log('i can bay secend hand laptop')
+// }
+
+// 8.problem
+// for(var i=1; i<=39; i++){
+//      console.log('ajke amar mon valo ney')
+// }
+
+// 9.problem
+// for(var i=58; i<=98; i++){
+//      console.log(i)
+// }
+
+// 10.problem
+// var num = 412;
+// while(num <= 456){
+//      console.log(num)
+//      num +=2;
+// }
+
+// 11.problem
+// for(var i=581; i<=623; i +=2){
+//      console.log(i)
+// } 
+// var corseModiul =['Html', 'bootstrap', 'Css', 'javascript'];
+// for(var i=0; i<=corseModiul.length; i++){
+//      console.log(corseModiul[i])
+// } 
+
+// 12.problem
+// var phone = ['samsang', 'shawmi', 'symphoni'];
+// var count = 0;
+// while(count < phone.length){
+//      console.log(phone[count]);
+//      count++;
+// }
+
+// 13.problem
+// var bookPrice = [100, 65, 150, 180, 190, 200, 300, 400];
+// for(var i = 0; i< bookPrice.length; i++){
+//      if(bookPrice[i] > 200){
+//           continue;         
+//      };
+//      console.log(bookPrice[i])
+// }
+
+//14.problem
+// function foo(){
+//      console.log(foo);
+// }
+// foo()
+
+// function bar(){
+//      console.log(bar);
+//      foo()
+// }
+// bar()
+
+// function make_avg(num1, num2, num3){
+//      totalParamiatr = num1 + num2 + num3;
+//      avarageNumber = totalParamiatr / 3;
+//      backAvarageNumber = parseFloat(avarageNumber);
+//      console.log(backAvarageNumber);
+    
+// }
+
+// make_avg(45, 20, 15);
+
+//15.problem
+// inchs to convert fit
+// const myInches = 12;
+// const myFeet = myInches / 12;
+// console.log(myFeet);
+
+// convert inch to feet
+// const dataInches = 144;
+// const dataFeet = dataInches /12;
+// console.log('data feet', dataFeet)
+
+// convert inch to feet
+// const dadiInches = 60;
+// const dadiFeet = dadiInches / 12;
+// console.log('dadiFeet', dadiFeet)
+
+//inch to feet convater
+// function inchToFeet(inches){
+//      const feet = inches / 12;
+//      return feet;
+// }
+
+// const dadaInches = 144;
+// const dadaFeet = inchToFeet(dadaInches);
+// console.log(dadaFeet);
+
+// const nanaInches = 168;
+// const nanaFeet = inchToFeet(nanaInches);
+// console.log('nana feet',nanaFeet);
+
+
+//16. mils to kilomitar js
+// function milsTokilomitar(mils){
+//      const kiloMitar = mils / 1.609;
+//      return kiloMitar;
+// }
+
+// const milsToConvert = 1500;
+// const kiloMitar = milsTokilomitar(mils);
+// console.log(kiloMitar)
+
+// const milsToConvert = 1400;
+// const kiloMitar = milsTokilomitar(milsToConvert);
+// console.log(kiloMitar)
+
+//mils to km converter
+// function milsToConvert(mils){
+//      kilomiter = mils*1.606;
+//      return kilomiter;
+// }
+
+// const milsToKm = 2500;
+// const kiloMitar = milsToConvert(milsToKm);
+// console.log(kiloMitar)
+
+//16.problem even and odd number cheker
+
+// function evenAndOdd(number){
+//    const reminder = number % 2;
+//      if(reminder === 0){
+//           console.log('it is a even number')
+//           return true;
+//      }
+//      else{
+//           return false;
+//      }
+// }
+
+// const inputNumber = 780;
+// const evenOrOd = evenAndOdd(inputNumber);
+// console.log(inputNumber +'number is '+evenOrOd)
+
+
+//17.problem check leap year or not
+
+// function leapYear(year){
+//      const remainder = year % 4;
+//      if(remainder=== 0){
+//           return true
+//      }
+//      else{
+//           return false
+//      }
+// }
+
+// const yearValue = 2024;
+// const leapYearOrNot = leapYear(yearValue);
+// console.log(leapYearOrNot);
+
+
+//another way 
+
+// function leapYear(year){
+//      reminder1 = year % 4;
+//      reminder2 = year % 100;
+//      reminder3 = year % 400;
+//      if((reminder1 === 0) && (reminder2 != 0)||reminder3 === 0){
+//           return true;
+//      }
+//      else{
+//           return false;
+//      }   
+// }
+// const GivenYear = 2065;
+// const leapYearOrNot = leapYear(GivenYear);
+// console.log(leapYearOrNot)
+
+//18.problem arry even and odd number access
+
+
+
+
+
